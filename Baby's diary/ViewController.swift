30c3a3a29f7858674a15860a6def0df5c4860e67
Sorokin Dmitriy <dmitrijsorokin532@gmail.com>
2024-06-19 07:02:28 -0700

    Initial Commit

---
+++ Baby's diary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Baby's diary
+//
+//  Created by mac on 19.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
