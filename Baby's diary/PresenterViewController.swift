2138fcad78c4386308672afc08e21398d52a00b1
Sorokin Dmitriy <dmitrijsorokin532@gmail.com>
2024-06-19 11:25:23 -0700

    Изменение начального представления

--- Baby's diary/PresenterViewController.swift
+++ Baby's diary/PresenterViewController.swift
@@ -7,35 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class PresenterViewController: UIViewController {
     
-    lazy var imageView: UIImageView = {
-        let image = UIImageView ()
-        image.backgroundColor = .cyan
+    private lazy var imageView: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: "baby")
         image.translatesAutoresizingMaskIntoConstraints = false
         image.contentMode = .scaleAspectFit
         return image
     }()
     
-    lazy var labelOne: UILabel = {
+    private lazy var labelOne: UILabel = {
         let label = UILabel()
         label.text = "Hi there!"
         label.numberOfLines = 0
         label.textAlignment = .center
-        label.font = UIFont.systemFont(ofSize: 23)
+        label.font = UIFont.systemFont(ofSize: 23, weight: .bold)
         return label
     }()
     
-    lazy var labelTwo: UILabel = {
+    private lazy var labelTwo: UILabel = {
         let label = UILabel()
         label.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
         label.numberOfLines = 0
         label.textAlignment = .center
-        label.font = UIFont.systemFont(ofSize: 16)
+        label.font = UIFont.italicSystemFont(ofSize: 16)
         return label
     }()
     
-    lazy var verticalStackView: UIStackView = {
+    private lazy var verticalStackView: UIStackView = {
         let stack = UIStackView()
         stack.axis = .vertical
         stack.spacing = 20
@@ -44,7 +44,7 @@ class ViewController: UIViewController {
         return stack
     }()
     
-    lazy var verticalStackViewOne: UIStackView = {
+    private lazy var verticalStackViewOne: UIStackView = {
         let stack = UIStackView()
         stack.axis = .vertical
         stack.spacing = 20
@@ -74,7 +74,7 @@ class ViewController: UIViewController {
         
         
         NSLayoutConstraint.activate([
-            imageView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9),
+            imageView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.7),
             imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor),
             
             verticalStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
